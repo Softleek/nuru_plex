21f4b21cd2f15c2af25b1acd3ed726ab0e97dddb
davidgitau <david@upeosoft.com>
2024-03-13 18:11:01 +0300

    Lo

--- nuru_plex/nuru_plex/doctype/house/house.js
+++ nuru_plex/nuru_plex/doctype/house/house.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, Nuru and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Property Details", {
+// frappe.ui.form.on("House", {
 // 	refresh(frm) {
 
 // 	},
--- nuru_plex/nuru_plex/doctype/property/property.js
+++ nuru_plex/nuru_plex/doctype/property/property.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2024, Nuru and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Property", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Property", {
+    refresh(frm) {
+      var currentUser = frappe.session.user;
+      frappe.call({
+        method: "frappe.client.get_list",
+        args: {
+          doctype: "Owner",
+          filters: {
+            user: currentUser
+          },
+          fields: ["name"]
+        },
+        callback: function(response) {
+          if (response.message && response.message.length > 0) {
+            var ownerName = response.message[0].name;
+            console.log(ownerName, currentUser);
+            frm.set_value("landlord", ownerName);
+          }
+        }
+      });
+    }
+  });
+  
